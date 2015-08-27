17563db51de62ac97f32fe489bb10c4c1deb5122
kayrus <kay.diam@gmail.com>
2015-08-27 17:41:26 +0200

    Added possibility to substitute network interfaces IP addresses

--- initialize/env.go
+++ initialize/env.go
@@ -15,12 +15,14 @@
 package initialize
 
 import (
+	"fmt"
 	"net"
 	"os"
 	"path"
 	"regexp"
 	"strings"
 
+	"github.com/coreos/coreos-cloudinit/Godeps/_workspace/src/github.com/docker/libcontainer/netlink"
 	"github.com/coreos/coreos-cloudinit/config"
 	"github.com/coreos/coreos-cloudinit/datasource"
 	"github.com/coreos/coreos-cloudinit/system"
@@ -28,12 +30,17 @@ import (
 
 const DefaultSSHKeyName = "coreos-cloudinit"
 
+type EnvVal struct {
+	envName string
+	val     string
+}
+
 type Environment struct {
 	root          string
 	configRoot    string
 	workspace     string
 	sshKeyName    string
-	substitutions map[string]string
+	substitutions map[string]EnvVal
 }
 
 // TODO(jonboulle): this is getting unwieldy, should be able to simplify the interface somehow
@@ -44,12 +51,76 @@ func NewEnvironment(root, configRoot, workspace, sshKeyName string, metadata dat
 		}
 		return ip.String()
 	}
-	substitutions := map[string]string{
-		"$public_ipv4":  firstNonNull(metadata.PublicIPv4, os.Getenv("COREOS_PUBLIC_IPV4")),
-		"$private_ipv4": firstNonNull(metadata.PrivateIPv4, os.Getenv("COREOS_PRIVATE_IPV4")),
-		"$public_ipv6":  firstNonNull(metadata.PublicIPv6, os.Getenv("COREOS_PUBLIC_IPV6")),
-		"$private_ipv6": firstNonNull(metadata.PrivateIPv6, os.Getenv("COREOS_PRIVATE_IPV6")),
+
+	substitutions := make(map[string]EnvVal)
+
+	envMap := map[string]string{
+		"$public_ipv4":  "COREOS_PUBLIC_IPV4",
+		"$private_ipv4": "COREOS_PRIVATE_IPV4",
+		"$public_ipv6":  "COREOS_PUBLIC_IPV6",
+		"$private_ipv6": "COREOS_PRIVATE_IPV6",
 	}
+
+	for key, val := range envMap {
+		var envVal EnvVal
+		switch key {
+		case "$public_ipv4":
+			envVal = EnvVal{envName: val, val: firstNonNull(metadata.PublicIPv4, os.Getenv(val))}
+		case "$private_ipv4":
+			envVal = EnvVal{envName: val, val: firstNonNull(metadata.PrivateIPv4, os.Getenv(val))}
+		case "$public_ipv6":
+			envVal = EnvVal{envName: val, val: firstNonNull(metadata.PublicIPv6, os.Getenv(val))}
+		case "$private_ipv6":
+			envVal = EnvVal{envName: val, val: firstNonNull(metadata.PrivateIPv6, os.Getenv(val))}
+		}
+		substitutions[key] = envVal
+	}
+
+	// Populate system network interfaces
+	defaultIfaceName := getDefaultGatewayIfaceName()
+	interfaces, err := net.Interfaces()
+	if err == nil {
+		fmt.Printf("Fetching network interfaces info\n")
+		for _, iface := range interfaces {
+			addrs, err := iface.Addrs()
+			if err == nil {
+				ipv4 := 0
+				ipv6 := 0
+				for _, addr := range addrs {
+					ip, _, err := net.ParseCIDR(addr.String())
+					if err != nil {
+						fmt.Printf("Warning: Cannot parse '%s' CIDR\n", addr.String())
+					} else {
+						var varName string
+						IPseq := ""
+						if ip.To4() != nil {
+							if ipv4 > 0 {
+								IPseq = fmt.Sprintf("_%d", ipv4)
+							}
+							varName = fmt.Sprintf("IFACE_%s%s_IPV4", strings.Replace(strings.ToUpper(iface.Name), ".", "_", -1), IPseq)
+							ipv4++
+						} else if ip.To16() != nil {
+							if ipv6 > 0 {
+								IPseq = fmt.Sprintf("_%d", ipv6)
+							}
+							varName = fmt.Sprintf("IFACE_%s%s_IPV6", strings.Replace(strings.ToUpper(iface.Name), ".", "_", -1), IPseq)
+							ipv6++
+						} else {
+							fmt.Printf("Warning: Incorrect IP address '%s', skipping\n", ip.String())
+							continue
+						}
+						key := fmt.Sprintf("$%s", strings.ToLower(varName))
+						substitutions[key] = EnvVal{envName: varName, val: ip.String()}
+						if defaultIfaceName == iface.Name && ip.To4() != nil {
+							substitutions["$iface_default_ipv4"] = EnvVal{envName: "IFACE_DEFAULT_IPV4", val: ip.String()}
+						}
+						fmt.Printf("Found '%s' network interface with '%s' IP address\n", iface.Name, ip.String())
+					}
+				}
+			}
+		}
+	}
+
 	return &Environment{root, configRoot, workspace, sshKeyName, substitutions}
 }
 
@@ -82,7 +153,7 @@ func (e *Environment) Apply(data string) string {
 		replKey := strings.Replace(key, `$`, `$$`, -1)
 
 		// "key" -> "val"
-		data = regexp.MustCompile(`([^\\]|^)`+matchKey).ReplaceAllString(data, `${1}`+val)
+		data = regexp.MustCompile(`([^\\]|^)`+matchKey).ReplaceAllString(data, `${1}`+val.val)
 		// "\key" -> "key"
 		data = regexp.MustCompile(`\\`+matchKey).ReplaceAllString(data, replKey)
 	}
@@ -96,21 +167,33 @@ func (e *Environment) DefaultEnvironmentFile() *system.EnvFile {
 		}},
 		Vars: map[string]string{},
 	}
-	if ip, ok := e.substitutions["$public_ipv4"]; ok && len(ip) > 0 {
-		ef.Vars["COREOS_PUBLIC_IPV4"] = ip
-	}
-	if ip, ok := e.substitutions["$private_ipv4"]; ok && len(ip) > 0 {
-		ef.Vars["COREOS_PRIVATE_IPV4"] = ip
-	}
-	if ip, ok := e.substitutions["$public_ipv6"]; ok && len(ip) > 0 {
-		ef.Vars["COREOS_PUBLIC_IPV6"] = ip
-	}
-	if ip, ok := e.substitutions["$private_ipv6"]; ok && len(ip) > 0 {
-		ef.Vars["COREOS_PRIVATE_IPV6"] = ip
+	for _, val := range e.substitutions {
+		if len(val.val) > 0 {
+			ef.Vars[val.envName] = val.val
+		}
 	}
 	if len(ef.Vars) == 0 {
 		return nil
 	} else {
 		return &ef
 	}
+}
+
+func getDefaultGatewayIfaceName() string {
+	routes, err := netlink.NetworkGetRoutes()
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		return ""
+	}
+	for _, route := range routes {
+		if route.Default {
+			if route.Iface == nil {
+				fmt.Printf("Warning: found default route but could not determine interface\n")
+				return ""
+			}
+			return route.Iface.Name
+		}
+	}
+	fmt.Printf("Warning: unable to find default route\n")
+	return ""
 }
